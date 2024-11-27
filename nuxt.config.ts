2649f2bb7a5959d20e2d46099d4130498e661ffd
Moussa Clarke <moussaclarke@gmail.com>
2024-11-27 13:35:27 +0000

    style: basic styling
    colours based on arboria so not fit for this project yet

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,5 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ["@unocss/nuxt"],
+  css: ["~/assets/scss/_reset.scss", "~/assets/scss/app.scss"],
 })
