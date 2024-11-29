f54a35b2a6bc1cbfa03ac3c332a900d50685746e
Moussa Clarke <moussaclarke@gmail.com>
2024-11-29 10:23:09 +0000

    refactor: extract out getTodos

--- server/api/backlog.ts
+++ server/api/backlog.ts
@@ -3,37 +3,9 @@ import davClient from "~/utils/davClient";
 
 export default defineEventHandler(async (event) => {
   const config = useRuntimeConfig();
+  const todos = await davClient().getTodos();
 
-  try {
-    const client = await davClient().getDavClient();
-    const calendars = await client.fetchCalendars();
-    const calendar = calendars.find(
-      (calendar) => calendar.displayName === config.davCalName,
-    );
-    if (!calendar) {
-      return {
-        statusCode: 404,
-        body: "Calendar not found",
-      };
-    }
-
-    const todos = await client.fetchCalendarObjects({
-      calendar,
-      filters: [
-        {
-          "comp-filter": {
-            _attributes: { name: "VCALENDAR" },
-            "comp-filter": {
-              _attributes: { name: "VTODO" },
-            },
-          },
-        },
-      ],
-      headers: await davClient().getCalendarHeaders(),
-    });
-
-    return todos;
-  } catch (error) {
-    return error;
+  return {
+    data: todos
   }
 });
--- utils/davClient.ts
+++ utils/davClient.ts
@@ -6,22 +6,71 @@ let incomingDigest: { scheme: string, realm: string, nonce: string } | undefined
 
 export default function () {
 
-  return {
-    getCalendarHeaders: async () => {
-      const config = useRuntimeConfig();
-      if (!client || !incomingDigest) {
+  const getCalendarHeaders = async () => {
+    const config = useRuntimeConfig();
+    await getDavClient();
+
+    if (!client || !incomingDigest) {
+      throw createError({
+        statusCode: 401,
+        statusMessage: "Unable to authenticate with DAV server",
+      });
+    }
+
+    if (config.davAuthMethod === 'basic') {
+      return getBasicAuthHeaders({
+        username: config.davUser,
+        password: config.davPassword,
+      });
+    }
+
+    const digest = ClientDigestAuth.generateProtectionAuth(
+      incomingDigest,
+      config.davUser,
+      config.davPassword,
+      {
+        uri: config.davURI,
+        method: "REPORT",
+        counter: 1,
+      },
+    ).raw;
+
+    return {
+      Authorization: digest,
+    };
+  }
+
+  const getDavClient = async (): Promise<DAVClient> => {
+    if (client) {
+      return client;
+    }
+
+    const config = useRuntimeConfig();
+
+    if (config.davAuthMethod === 'basic') {
+      client = await createDAVClient({
+        serverUrl: config.davBase + config.davURI,
+        defaultAccountType: "caldav",
+        credentials: {
+          username: config.davUser,
+          password: config.davPassword,
+        },
+        authMethod: "Basic",
+      });
+
+      return client;
+    } else if (config.davAuthMethod === 'digest') {
+      const res = await fetch(config.davBase + config.davURI);
+      const authenticateHeader = res.headers.get("www-authenticate");
+
+      if (!authenticateHeader) {
         throw createError({
           statusCode: 401,
-          statusMessage: "Client not authenticated",
+          statusMessage: "Unauthorized",
         });
       }
 
-      if (config.davAuthMethod === 'basic') {
-        return getBasicAuthHeaders({
-          username: config.davUser,
-          password: config.davPassword,
-        });
-      }
+      incomingDigest = ClientDigestAuth.analyze(authenticateHeader);
 
       const digest = ClientDigestAuth.generateProtectionAuth(
         incomingDigest,
@@ -29,87 +78,75 @@ export default function () {
         config.davPassword,
         {
           uri: config.davURI,
-          method: "REPORT",
+          method: "PROPFIND",
           counter: 1,
         },
-      ).raw;
+      ).raw.replace("Digest ", "");
 
-      return {
-        Authorization: digest,
-      };
-    },
-    getDavClient: async (): Promise<DAVClient> => {
-      if (client) {
-        return client;
-      }
-
-      const config = useRuntimeConfig();
-
-      if (config.davAuthMethod === 'basic') {
+      try {
         client = await createDAVClient({
           serverUrl: config.davBase + config.davURI,
           defaultAccountType: "caldav",
           credentials: {
-            username: config.davUser,
-            password: config.davPassword,
+            digestString: digest,
           },
-          authMethod: "Basic",
+          authMethod: "Digest",
         });
 
         return client;
-      } else if (config.davAuthMethod === 'digest') {
-        const res = await fetch(config.davBase + config.davURI);
-        const authenticateHeader = res.headers.get("www-authenticate");
-
-        if (!authenticateHeader) {
-          throw createError({
-            statusCode: 401,
-            statusMessage: "Unauthorized",
-          });
-        }
-
-        incomingDigest = ClientDigestAuth.analyze(authenticateHeader);
-
-        const digest = ClientDigestAuth.generateProtectionAuth(
-          incomingDigest,
-          config.davUser,
-          config.davPassword,
-          {
-            uri: config.davURI,
-            method: "PROPFIND",
-            counter: 1,
-          },
-        ).raw.replace("Digest ", "");
-
-        try {
-          client = await createDAVClient({
-            serverUrl: config.davBase + config.davURI,
-            defaultAccountType: "caldav",
-            credentials: {
-              digestString: digest,
-            },
-            authMethod: "Digest",
-          });
-
-          return client;
-        } catch (error) {
-          // coerce error to error type
-          if (error instanceof Error) {
-            throw createError(
-              {
-                statusCode: 500,
-                statusMessage: "Error authenticating with digest auth",
-                cause: error,
-              }
-            )
-          }
+      } catch (error) {
+        // coerce error to error type
+        if (error instanceof Error) {
+          throw createError(
+            {
+              statusCode: 500,
+              statusMessage: "Error authenticating with digest auth",
+              cause: error,
+            }
+          )
         }
       }
-
-      throw createError({
-        statusCode: 500,
-        statusMessage: "Unsupported auth method. Check your config.",
-      })
     }
+
+    throw createError({
+      statusCode: 500,
+      statusMessage: "Unsupported auth method. Check your config.",
+    })
+  }
+
+  const getTodos = async () => {
+    const client = await getDavClient();
+    const config = useRuntimeConfig();
+    const calendars = await client.fetchCalendars();
+    const calendar = calendars.find(
+      (calendar) => calendar.displayName === config.davCalName,
+    );
+    if (!calendar) {
+      throw createError({
+        statusCode: 404,
+        message: "Calendar not found",
+      });
+    }
+
+    const todos = await client.fetchCalendarObjects({
+      calendar,
+      filters: [
+        {
+          "comp-filter": {
+            _attributes: { name: "VCALENDAR" },
+            "comp-filter": {
+              _attributes: { name: "VTODO" },
+            },
+          },
+        },
+      ],
+      headers: await getCalendarHeaders(),
+    });
+
+    return todos;
+  }
+
+  return {
+    getTodos
   }
 }
