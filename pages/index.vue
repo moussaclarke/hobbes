e4acaf4ed1cb47ad1d113947425583cb78aaeb50
Moussa Clarke <moussaclarke@gmail.com>
2024-11-30 13:36:15 +0000

    feat: filter by status poc

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,33 @@
 <template>
   <div class="box">
-    <h1>All Tasks</h1>
-    <div class="grid">
-      <TaskCard v-for="task in data.data" :key="task.id" :task="task" />
+    <h1>{{ filter }} Tasks</h1>
+    <div class="cluster">
+      <button class="button small" @click="filter = 'All'">All</button>
+      <button class="button small" @click="filter = 'Todo'">Todo</button>
+      <button class="button small" @click="filter = 'Done'">Done</button>
+    </div>
+    <div class="grid constrained">
+      <TaskCard v-for="task in filteredData" :key="task.id" :task="task" />
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const { data } = await useFetch("/api/backlog");
+const filter: Ref<"All" | "Todo" | "Done"> = ref("All");
+const filteredData = computed(() => {
+  if (filter.value === "All") {
+    return data.value.data;
+  }
+
+  if (filter.value === "Todo") {
+    return data.value.data.filter((task) => {
+      return task.status !== "COMPLETED" && task.status !== "CANCELLED";
+    });
+  }
+
+  return data.value.data.filter((task) => {
+    return task.status === "COMPLETED" || task.status === "CANCELLED";
+  });
+});
 </script>
