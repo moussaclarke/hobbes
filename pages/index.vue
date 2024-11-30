65d23fb06d1852bc9d2d34c7cbcb2a9226075f8a
Moussa Clarke <moussaclarke@gmail.com>
2024-11-30 10:18:18 +0000

    feat: task list poc

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <h1>Hello World</h1>
+    <ul>
+      <li v-for="task in data.data" :key="task.id">
+        {{ task.summary }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data } = await useFetch("/api/backlog");
+</script>
--- server/api/backlog.ts
+++ server/api/backlog.ts
@@ -1,9 +1,9 @@
 import davClient from "~/utils/davClient";
-import { parseTask } from "~/utils/parseTasks";
+import { parseTask } from "~/utils/parseTask";
 
 export default defineEventHandler(async () => {
   const rawTasks = await davClient().getTasks();
-  const tasks: Task[] = rawTasks.map(parseTask);
+  const tasks: Task[] = rawTasks.map(parseTask).sort((a, b) => b.created.getTime() - a.created.getTime());
 
   return {
     data: tasks,
