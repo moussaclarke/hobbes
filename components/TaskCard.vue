f9271993edd8ffeb1f28d9f7b8444b857b55f471
Moussa Clarke <moussaclarke@gmail.com>
2024-11-30 13:13:24 +0000

    chore: wrap long words

--- components/TaskCard.vue
+++ components/TaskCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="stack flow box border border-solid border-primary rounded">
     <h2 class="medium primary">{{ props.task.summary }}</h2>
-    <p class="small">{{ props.task.description }}</p>
+    <p class="small prose">{{ props.task.description }}</p>
     <div class="cluster">
       <CategoryTag
         v-for="category in props.task.categories"
