dca26bae0fc2e5db44d607af30ef22640514c0ae
Moussa Clarke <moussaclarke@gmail.com>
2024-12-02 14:16:37 +0000

    feat: required fields

--- pages/form.vue
+++ pages/form.vue
@@ -28,6 +28,7 @@
             v-model="issueType"
             name="issue-type"
             id="issue-type"
+            required
           >
             <option value="bug">Bug</option>
             <option value="change">Change Request</option>
@@ -46,6 +47,7 @@
             v-model="priority"
             name="priority"
             id="priority"
+            required
           >
             <option value="low">Low</option>
             <option value="medium">Medium</option>
@@ -58,13 +60,14 @@
         </div>
       </div>
       <div class="form-group">
-        <label for="summary">Title</label>
+        <label for="summary">Title *</label>
         <input
           class="w-full"
           type="text"
           v-model="summary"
           name="summary"
           id="summary"
+          required
         />
         <small>
           A concise summary of the issue. It's often more useful to try to
@@ -73,12 +76,13 @@
         </small>
       </div>
       <div class="form-group">
-        <label for="description">Description</label>
+        <label for="description">Description *</label>
         <textarea
           class="w-full"
           v-model="description"
           name="description"
           id="description"
+          required
         ></textarea>
         <small
           v-html="
@@ -88,12 +92,13 @@
         ></small>
       </div>
       <div class="form-group">
-        <label for="context">Context</label>
+        <label for="context">Context *</label>
         <textarea
           class="w-full"
           v-model="context"
           name="context"
           id="context"
+          required
         ></textarea>
         <small
           v-html="'Please provide some additional context: ' + contextHelpText"
@@ -102,9 +107,10 @@
       <button class="button" type="submit">Submit</button>
     </form>
     <small>
-      Tip: If you need to include screenshots, try
-      <a target="_blank" href="https://snipboard.io">snipboard</a>. For screen
-      recordings, you can upload them to dropbox, google drive or similar.
+      Tip: If you need to include any screenshots, try
+      <a target="_blank" href="https://snipboard.io">snipboard.io</a>. For
+      screen recordings, you can upload them to dropbox, google drive or similar
+      then paste the link.
     </small>
   </div>
 </template>
