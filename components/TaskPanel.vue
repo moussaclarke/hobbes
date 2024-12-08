8408bf0297028b67b6dedfafc6bacc416d03141a
Moussa Clarke <moussaclarke@gmail.com>
2024-12-08 08:06:41 +0000

    refactor: extract task meta

--- components/TaskPanel.vue
+++ components/TaskPanel.vue
@@ -23,59 +23,16 @@
         />
       </div>
     </div>
-    <div class="stack flow">
-      <div class="cluster">
-        <CategoryTag
-          v-for="category in props.task?.categories"
-          :key="category"
-          :category="category"
-        />
-      </div>
-      <div class="stack text-right border-t border-x-0 border-solid py-1">
-        <strong class="x-small">
-          {{ status }}
-        </strong>
-        <span class="x-small">
-          Created: {{ taskWithDates?.created.toLocaleDateString() }}
-        </span>
-        <span class="x-small">
-          Last modified: {{ taskWithDates?.lastModified.toLocaleDateString() }}
-        </span>
-        <span class="x-small" v-if="taskWithDates?.organizer">
-          Opened by: {{ taskWithDates?.organizer }}
-        </span>
-      </div>
-    </div>
+    <TaskMeta :task="props.task" />
   </div>
 </template>
 <script setup lang="ts">
 import { useDescriptionRenderer } from "~/composables/useDescriptionRenderer";
 const props = defineProps<{ task: Task | null }>();
-const taskWithDates = computed(() => props.task && useTaskDates(props.task));
 const { render } = useDescriptionRenderer();
 
 const description = computed(() => {
   if (!props.task?.description) return { content: "", comments: [] };
   return render(props.task.description);
-});
-
-const statusFormatters = {
-  COMPLETED: (task: Task) =>
-    `Marked as completed on ${task.completed?.toLocaleDateString()}`,
-  CANCELLED: () => "This task was cancelled",
-  "NEEDS-ACTION": () => "This task is open",
-  "IN-PROCESS": () => "This task is in progress",
-};
-
-const status = computed(() => {
-  if (!props.task?.status) {
-    return "No status assigned";
-  }
-
-  const formatter = statusFormatters[props.task.status];
-  if (formatter && taskWithDates.value) {
-    return formatter(taskWithDates.value);
-  }
-  return props.task.status;
 });
 </script>
