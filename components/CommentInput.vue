738a8317e12b77c8f7a83993d9eb2e1d8bd3a209
Moussa Clarke <moussaclarke@gmail.com>
2024-12-09 07:49:50 +0000

    refactor: proper comment form ele

--- components/CommentInput.vue
+++ components/CommentInput.vue
@@ -1,14 +1,15 @@
 <template>
-  <div class="stack flow">
+  <form class="stack flow" @submit.prevent="addComment">
     <div class="form-group">
       <textarea
         v-model="comment"
         class="w-full"
         placeholder="Add a comment..."
+        required
       />
     </div>
-    <button class="button" @click="addComment">Add Comment</button>
-  </div>
+    <button class="button">Add Comment</button>
+  </form>
 </template>
 <script setup lang="ts">
 const props = defineProps<{ task: Task }>();
@@ -16,8 +17,6 @@ const props = defineProps<{ task: Task }>();
 const comment = ref("");
 
 const addComment = async () => {
-  // todo
-  console.log("add comment for task", props.task.id);
   const res = await $fetch("/api/comment", {
     method: "POST",
     body: {
