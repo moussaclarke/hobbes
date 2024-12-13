f6be1abf9f06576140e5e261988a10364a872c46
Moussa Clarke <moussaclarke@gmail.com>
2024-12-13 08:25:42 +0000

    chore: logo to footer

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,8 +1,11 @@
 <template>
   <footer class="footer container w-full">
-    <small>
-      Hobbes - A Project Management Tool. Copyright © 2024
-      <a href="https://moussaclarke.dev">Moussa Clarke</a>
+    <small class="cluster">
+      <img class="h-2em" src="~/assets/img/logo.svg" alt="Hobbes Logo" />
+      <span>
+        Hobbes - A Project Management Tool. Copyright © 2024
+        <a href="https://moussaclarke.dev">Moussa Clarke</a>
+      </span>
     </small>
   </footer>
 </template>
